7c8b22042c60557a08fee928b40bc825483977a1
Fraser Cook <fraserc6@gmail.com>
2020-09-06 22:55:43 +0100

    Button height typo

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
                                     className="flex items-center px-3 py-2 text-sm rounded-md duration-100 transition-colors bg-gray-800 text-white hover:bg-gray-700"
                                 >
                                     <span className="hidden sm:flex">Create Account</span>
-                                    <svg viewBox="0 0 20 20" fill="currentColor" className="arrow-right w-6 h-6 sm:w-4 sm:h-6 sm:ml-2">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" className="arrow-right w-6 h-6 sm:w-4 sm:h-4 sm:ml-2">
                                         <path
                                             fillRule="evenodd"
                                             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
